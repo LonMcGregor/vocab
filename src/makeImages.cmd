d535e3f94263badfe4e56da79b86c4009bc6c614
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2018-09-22 10:07:04 +0100

    icon

---
+++ src/makeImages.cmd
@@ -0,0 +1,9 @@
+rem http://tavmjong.free.fr/INKSCAPE/MANUAL/html/CommandLine-Export.html
+"%PROGRAMFILES%\Inkscape\inkscape.exe" -e=./public/16.png -C -w=16 -h=16 ./src/icon.svg
+"%PROGRAMFILES%\Inkscape\inkscape.exe" -e=./public/favicon.png -C -w=16 -h=16  ./src/icon.svg
+"%PROGRAMFILES%\Inkscape\inkscape.exe" -e=./public/24.png -C -w=24 -h=24  ./src/icon.svg
+"%PROGRAMFILES%\Inkscape\inkscape.exe" -e=./public/32.png -C -w=32 -h=32  ./src/icon.svg
+"%PROGRAMFILES%\Inkscape\inkscape.exe" -e=./public/48.png -C -w=48 -h=48  ./src/icon.svg
+"%PROGRAMFILES%\Inkscape\inkscape.exe" -e=./public/128.png -C -w=128 -h=128  ./src/icon.svg
+"%PROGRAMFILES%\Inkscape\inkscape.exe" -e=./public/256.png -C -w=256 -h=256  ./src/icon.svg
+"%PROGRAMFILES%\Inkscape\inkscape.exe" -e=./public/512.png -C -w=512 -h=512  ./src/icon.svg
